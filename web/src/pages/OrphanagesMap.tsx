293c2cf32f4d11037633c018d02ac1a740446f0d
thiirush94 <thiago.pereira@komuh.com>
2020-10-16 19:38:01 -0300

    Backend finalizado e metade do frontweb.

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+
+import mapMarkerImg from "../images/Local.svg";
+
+import "../styles/pages/orphanages-map.css";
+import { mapIcon } from "../utils/mapIcon";
+
+function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Rio do Sul</strong>
+          <span>Santa Catarina</span>
+        </footer>
+      </aside>
+      <Map
+        center={[-19.9460571, -43.9150582]}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+        <Marker icon={mapIcon} position={[-19.9460571, -43.9150582]}>
+          <Popup
+            minWidth={240}
+            maxWidth={240}
+            closeButton={false}
+            className="map-popup"
+          >
+            Lar das meninas
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
+      </Map>
+      s
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
